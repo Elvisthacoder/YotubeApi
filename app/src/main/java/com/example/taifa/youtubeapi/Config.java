1a38192b1d3d93eadf56ced01aea57b09dba9d37
Elvisthacoder <ekk213@gmail.com>
2016-03-13 19:37:32 +0300

    adding config java class

--- app/src/main/java/com/example/taifa/youtubeapi/Config.java
+++ app/src/main/java/com/example/taifa/youtubeapi/Config.java
@@ -4,4 +4,9 @@ package com.example.taifa.youtubeapi;
  * Created by taifa on 3/13/16.
  */
 public class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "YOUR API KEY";
 }
